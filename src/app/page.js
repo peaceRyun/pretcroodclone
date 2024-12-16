3afa4eff217a74b6b90aa3470dea7994b8e9eae8
dbsrn1110 <dbsrn1110@gmail.com>
2024-12-16 11:44:29 +0900

    global => css에서 scss로 변경/ font, reset 추가 및 테스트트

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
 import Image from 'next/image';
 
 export default function Home() {
-    return <>ddd</>;
+    return (
+        <>
+            <div className='flex flex-col gap-3 uppercase'>
+                <span className='font-proxima font-mobile-display1'>Set the trend</span>
+                <span className='font-proxima font-mobile-display1'>in digital fashion</span>
+            </div>
+        </>
+    );
 }
