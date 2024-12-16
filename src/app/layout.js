cff2681f61dfa172c5794098b24e7cac63b72e2d
dbsrn1110 <dbsrn1110@gmail.com>
2024-12-16 12:20:31 +0900

    gsap 라이브러리 설치 및 테스트 / page => h2 태그 제작작

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,10 @@
+'use client';
+
 import { Geist, Geist_Mono } from 'next/font/google';
 import './styles/globals.scss';
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -11,30 +16,12 @@ const geistMono = Geist_Mono({
     subsets: ['latin'],
 });
 
-export const metadata = {
-    title: 'PRETCROOD CloneCoding',
-    description: 'PRETCROOD 클론코딩 프로젝트입니다.',
-    openGraph: {
-        type: 'website',
-        title: '페이지 제목',
-        description: '페이지 설명',
-        url: 'http://www.mysite.com/article/article1.html',
-        images: [
-            {
-                url: 'http://www.mysite.com/article/article1_featured_image.jpg',
-            },
-        ],
-    },
-    twitter: {
-        card: 'summary',
-        title: '페이지 제목',
-        description: '페이지 설명',
-        images: ['http://www.mysite.com/article/article1.html'],
-        domain: '사이트 명',
-    },
-};
-
 export default function RootLayout({ children }) {
+    useEffect(() => {
+        // GSAP 및 ScrollTrigger 등록
+        gsap.registerPlugin(ScrollTrigger);
+    }, []);
+
     return (
         <html lang='ko'>
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
---
+++ src/app/metadata.js
@@ -0,0 +1,22 @@
+export const metadata = {
+    title: 'PRETCROOD CloneCoding',
+    description: 'PRETCROOD 클론코딩 프로젝트입니다.',
+    openGraph: {
+        type: 'website',
+        title: '페이지 제목',
+        description: '페이지 설명',
+        url: 'http://www.mysite.com/article/article1.html',
+        images: [
+            {
+                url: 'http://www.mysite.com/article/article1_featured_image.jpg',
+            },
+        ],
+    },
+    twitter: {
+        card: 'summary',
+        title: '페이지 제목',
+        description: '페이지 설명',
+        images: ['http://www.mysite.com/article/article1.html'],
+        domain: '사이트 명',
+    },
+};
