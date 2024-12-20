6841c965d8efb9afe2523452074d93de8e6c7c14
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-20 16:23:00 +0900

    withsection => 구현4(card 빈곳 눌렀을 때 모두 비활성화 구현)

--- src/app/home/components/WithSection.jsx
+++ src/app/home/components/WithSection.jsx
@@ -1,17 +1,31 @@
 // WithSection.jsx
 import MiniCard from '@/app/components/card/Card';
 import { WITH_SECTION_DATA } from '@/app/data/texts';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const WithSection = () => {
     const [activeId, setActiveId] = useState(null);
+    const sectionRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (sectionRef.current && !sectionRef.current.contains(event.target)) {
+                setActiveId(null); // 외부 클릭 시 모든 카드 닫기
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return (
         <div className='flex flex-col gap-20 py-24 px-3'>
             <h2 className='mx-auto uppercase font-proxima text-white font-mobile-display2'>
                 <strong>do it with us</strong>
             </h2>
-            <ul className='flex flex-col gap-3'>
+            <ul ref={sectionRef} className='flex flex-col gap-3'>
                 {WITH_SECTION_DATA.map((item) => (
                     <li key={item.id}>
                         <MiniCard
