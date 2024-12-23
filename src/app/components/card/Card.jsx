3bc86ddffccf2413c1abf7025980b894c7b54d05
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-23 17:34:37 +0900

    withsection => gsap 속성 추가 및 삭제

--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import gsap from 'gsap';
 
-const MiniCard = ({ subtitle, p, isActive, onMouseEnter, onMouseLeave }) => {
+const MiniCard = ({ subtitle, p, onMouseEnter, onMouseLeave }) => {
     const cardRef = useRef(null);
     const titleRef = useRef(null);
     const paragraphRef = useRef(null);
@@ -10,8 +10,9 @@ const MiniCard = ({ subtitle, p, isActive, onMouseEnter, onMouseLeave }) => {
         // Initial setup
         gsap.set(paragraphRef.current, {
             opacity: 0,
-            height: 0,
+            // height: 0,
             display: 'none',
+            duration: 0.3,
         });
     }, []);
 
@@ -28,7 +29,7 @@ const MiniCard = ({ subtitle, p, isActive, onMouseEnter, onMouseLeave }) => {
                 backgroundColor: '#ffffff',
                 color: '#000000',
                 duration: 0.3,
-                height: '200px',
+                // height: '200px',
                 ease: 'power2.out',
             })
             .to(
@@ -45,7 +46,7 @@ const MiniCard = ({ subtitle, p, isActive, onMouseEnter, onMouseLeave }) => {
                 paragraphRef.current,
                 {
                     opacity: 1,
-                    height: '50px',
+                    // height: '75px',
                     duration: 0.3,
                     ease: 'power2.out',
                 },
@@ -67,7 +68,7 @@ const MiniCard = ({ subtitle, p, isActive, onMouseEnter, onMouseLeave }) => {
             .to(cardRef.current, {
                 backgroundColor: '#18181b', // zinc-900
                 color: '#ffffff',
-                height: 'auto',
+                // height: '0px',
                 duration: 0.3,
                 ease: 'power2.out',
             })
@@ -84,7 +85,7 @@ const MiniCard = ({ subtitle, p, isActive, onMouseEnter, onMouseLeave }) => {
             .to(
                 paragraphRef.current,
                 {
-                    opacity: 0,
+                    opacity: 1,
                     duration: 0.3,
                     ease: 'power2.out',
                 },
@@ -97,14 +98,16 @@ const MiniCard = ({ subtitle, p, isActive, onMouseEnter, onMouseLeave }) => {
     return (
         <div
             ref={cardRef}
-            className='block rounded-3xl w-full text-left lg:flex lg:items-start lg:gap-4 p-7 lg:p-14 bg-zinc-900 text-white transition-all duration-0'
+            className='block rounded-3xl w-full text-left p-7 lg:flex lg:items-start lg:gap-4  lg:p-14 bg-zinc-900 text-white'
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
         >
-            <h3 ref={titleRef} className='font-proxima font-display3-desktop lg:w-1/2 origin-left'>
-                {subtitle}
-            </h3>
-            <p ref={paragraphRef} className='font-proxima-normal font-bold text-sm leading-6 lg:w-1/2'>
+            <div className='titlebox '>
+                <h3 ref={titleRef} className='font-proxima font-display3-desktop lg:w-1/2 origin-left'>
+                    {subtitle}
+                </h3>
+            </div>
+            <p ref={paragraphRef} className='font-proxima-normal font-bold text-xs leading-6 lg:w-1/2 lg:text-lg'>
                 {p}
             </p>
         </div>
