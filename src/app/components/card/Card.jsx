09821e2c30fb3665ff430362289578c147e97b2b
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-23 16:39:30 +0900

    withsection => 카드 애니메이션 높이값 수정, font-proxima-normal 추가

--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -10,6 +10,7 @@ const MiniCard = ({ subtitle, p, isActive, onMouseEnter, onMouseLeave }) => {
         // Initial setup
         gsap.set(paragraphRef.current, {
             opacity: 0,
+            height: 0,
             display: 'none',
         });
     }, []);
@@ -27,6 +28,7 @@ const MiniCard = ({ subtitle, p, isActive, onMouseEnter, onMouseLeave }) => {
                 backgroundColor: '#ffffff',
                 color: '#000000',
                 duration: 0.3,
+                height: '200px',
                 ease: 'power2.out',
             })
             .to(
@@ -43,6 +45,7 @@ const MiniCard = ({ subtitle, p, isActive, onMouseEnter, onMouseLeave }) => {
                 paragraphRef.current,
                 {
                     opacity: 1,
+                    height: '50px',
                     duration: 0.3,
                     ease: 'power2.out',
                 },
@@ -64,6 +67,7 @@ const MiniCard = ({ subtitle, p, isActive, onMouseEnter, onMouseLeave }) => {
             .to(cardRef.current, {
                 backgroundColor: '#18181b', // zinc-900
                 color: '#ffffff',
+                height: 'auto',
                 duration: 0.3,
                 ease: 'power2.out',
             })
@@ -93,14 +97,14 @@ const MiniCard = ({ subtitle, p, isActive, onMouseEnter, onMouseLeave }) => {
     return (
         <div
             ref={cardRef}
-            className='block rounded-3xl w-full text-left lg:flex lg:items-start lg:gap-4 p-7 lg:p-14 bg-zinc-900 text-white'
+            className='block rounded-3xl w-full text-left lg:flex lg:items-start lg:gap-4 p-7 lg:p-14 bg-zinc-900 text-white transition-all duration-0'
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
         >
             <h3 ref={titleRef} className='font-proxima font-display3-desktop lg:w-1/2 origin-left'>
                 {subtitle}
             </h3>
-            <p ref={paragraphRef} className='font-proxima font-bold text-sm leading-6 lg:w-1/2'>
+            <p ref={paragraphRef} className='font-proxima-normal font-bold text-sm leading-6 lg:w-1/2'>
                 {p}
             </p>
         </div>
