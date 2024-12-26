ca3ff110fa1022c6de407a2d1357163e88275642
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-26 09:36:39 +0900

    slidesection => 작업1

--- src/app/home/components/SlideSection.jsx
+++ src/app/home/components/SlideSection.jsx
@@ -4,7 +4,31 @@ import Image from 'next/image';
 import React from 'react';
 
 const SlideSection = () => {
-    return <></>;
+    return (
+        <>
+            <section className='flex flex-col gap-20 px-5'>
+                <div className='title-area flex'>
+                    <p className='font-proxima font-display3-h2 text-white uppercase mx-auto'>how it works</p>
+                </div>
+                <div className='img-slide-wrap'>
+                    <ul className='img-slides flex gap-5'>
+                        {[1, 2, 3, 4, 5].map((num) => (
+                            <li key={num}>
+                                <div className='relative w-full aspect-[5/7]'>
+                                    <Image
+                                        src={`/images/pattern/home/slidesection/card_image_${num}.png`}
+                                        alt={`card${num}`}
+                                        fill
+                                        className='w-[500px] h-[700px] object-cover'
+                                    />
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default SlideSection;
