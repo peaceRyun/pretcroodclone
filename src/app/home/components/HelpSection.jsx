75af662c83e7082f868f9b09b4ed2e55408cdb8a
dbsrn1110 <dbsrn1110@gmail.com>
2024-12-26 21:44:32 +0900

    helpsection=> 작업3(반응형)

--- src/app/home/components/HelpSection.jsx
+++ src/app/home/components/HelpSection.jsx
@@ -7,14 +7,14 @@ import React from 'react';
 const HelpSection = () => {
     return (
         <>
-            <section>
+            <section className='pb-[18.75vw]'>
                 <marquee behavior='scroll' direction='left'>
                     <div className='flex gap-4'>
                         <span className='font-proxima fs-160-80 text-white font-bold'>NEED MORE HELP?</span>
                         <Image src='/images/component/logo_only.svg' width={63} height={60} alt='로고 이미지 ' />
                     </div>
                 </marquee>
-                <div className='contact-wrap flex flex-col gap-5 px-5 '>
+                <div className='contact-wrap flex flex-col gap-10 px-5 justify-center md:flex-row'>
                     <div className='discord flex flex-col gap-2 p-5 items-center contact-radius bgc-g90'>
                         <DiscordIcon className='text-white' />
                         <h3 className='font-proxima text-white text-center font-bold fs-40-28'>Discord</h3>
@@ -26,12 +26,16 @@ const HelpSection = () => {
                             <ArrowRight color='gray' />
                         </Link>
                     </div>
-                    <div className='email flex flex-col gap-2 p-5 items-center bgc-g10'>
+                    <div className='email flex flex-col gap-2 p-5 items-center bgc-g10 rounded-2xl'>
                         <EmailIcon />
                         <h3 className='font-proxima text-center font-bold fs-40-28 color-g90'>help@pretcoord.com</h3>
                         <p className='font-proxima-normal color-g50 fs-20-16 text-center'>
                             Get a demo or advice to integrate garment tech into your process.
                         </p>
+                        <Link href='#' className='flex gap-2 mt-[50px]'>
+                            <strong className='font-proxima-normal color-g90 fs-20-14 text-center'>Contact Us</strong>
+                            <ArrowRight color='black' />
+                        </Link>
                     </div>
                 </div>
             </section>
