acb715e60c40e95cf7865f4f6b4e77f66e766158
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-26 17:45:15 +0900

    plansection => 반응형 레이아웃 수정

--- src/app/home/components/PlanSection.jsx
+++ src/app/home/components/PlanSection.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 const PlanSection = () => {
     return (
         <>
-            <section>
-                <div className='title-area flex flex-col'>
+            <section className='layout-plan-wrap'>
+                <div className='flex flex-col'>
                     <p className='font-proxima font-display3-h2 text-white uppercase mx-auto'>select a plan</p>
-                    <div className='mt-10 p-5 flex flex-col gap-5'>
+                    <div className='layout-plan mt-10 p-5 flex flex-col gap-5 lg:flex-row'>
                         <PlanCard />
                         <PlanCardWhite category='Standalone' />
                         <PlanCardWhite category='Float' />
