92588b582583eca887a3b1bbb133c8a2f67c3d6c
dbsrn1110 <dbsrn1110@gmail.com>
2024-12-29 20:30:23 +0900

    header => hover css 구현(depth2)

--- src/app/components/layout/header/Header.jsx
+++ src/app/components/layout/header/Header.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import { ButtonWhite } from '../../button/Button';
 import { HeaderLogo } from '../../logo/Logo';
-import { ArrowUpRight, ChevronDown } from 'lucide-react';
+import { ArrowUpRight, Asterisk, BookOpen, ChevronDown, MessageSquareText, Pencil, Send, Zap } from 'lucide-react';
 
 const Header = () => {
     return (
@@ -18,67 +18,123 @@ const Header = () => {
                     <div className='header-left'>
                         <nav>
                             <ul className='flex gap-4'>
-                                <li>
+                                <li className='menu-item relative py-3'>
                                     <Link href='/' className='flex items-center gap-1 group'>
                                         <span>Product</span>
                                         <ChevronDown color='white' size={20} className='chevron-rotate' />
                                     </Link>
                                     <div className='floating-menu bgc-o-90'>
-                                        <ul className='flex flex-col gap-4'>
+                                        <ul className='flex flex-col'>
                                             <li>
-                                                <Link href='/'>
-                                                    <span>Feature</span>
+                                                <Link
+                                                    href='/'
+                                                    className='flex gap-2 px-5 items-center text-center leading-10 group'
+                                                >
+                                                    <Asterisk
+                                                        size={17}
+                                                        className='color-g50 group-hover:text-[#2aea65]'
+                                                    />
+                                                    <span className='color-g50 group-hover:text-[#2aea65]'>
+                                                        Feature
+                                                    </span>
                                                 </Link>
                                             </li>
                                             <li>
-                                                <Link href='/'>
-                                                    <span>What' s New?</span>
+                                                <Link
+                                                    href='/'
+                                                    className='flex gap-2 px-5 items-center text-center whitespace-nowrap leading-10 group'
+                                                >
+                                                    <Zap size={17} className='color-g50 group-hover:text-[#2aea65]' />
+                                                    <span className='color-g50 group-hover:text-[#2aea65]'>
+                                                        What' s New?
+                                                    </span>
                                                 </Link>
                                             </li>
                                         </ul>
                                     </div>
                                 </li>
-                                <li>
+                                <li className='menu-item relative py-3'>
                                     <Link href='/' className='flex items-center gap-1 group'>
                                         <span>Learn</span>
                                         <ChevronDown color='white' size={20} className='chevron-rotate' />
                                     </Link>
                                     <div className='floating-menu bgc-o-90'>
-                                        <ul className='flex flex-col gap-4'>
+                                        <ul className='flex flex-col'>
                                             <li>
-                                                <Link href='/'>
-                                                    <span>Tutorial</span>
+                                                <Link
+                                                    href='/'
+                                                    className='flex gap-2 px-5 items-center text-center leading-10 group'
+                                                >
+                                                    <Pencil
+                                                        size={17}
+                                                        className='color-g50 group-hover:text-[#2aea65]'
+                                                    />
+                                                    <span className='color-g50 group-hover:text-[#2aea65]'>
+                                                        Tutorial
+                                                    </span>
+                                                    <ArrowUpRight
+                                                        size={17}
+                                                        className='color-g50 group-hover:text-[#2aea65]'
+                                                    />
                                                 </Link>
                                             </li>
                                             <li>
-                                                <Link href='/'>
-                                                    <span>Manual</span>
+                                                <Link
+                                                    href='/'
+                                                    className='flex gap-2 px-5 items-center text-center leading-10 group'
+                                                >
+                                                    <BookOpen
+                                                        size={17}
+                                                        className='color-g50 group-hover:text-[#2aea65]'
+                                                    />
+                                                    <span className='color-g50 group-hover:text-[#2aea65]'>Manual</span>
+                                                    <ArrowUpRight
+                                                        size={17}
+                                                        className='color-g50 group-hover:text-[#2aea65]'
+                                                    />
                                                 </Link>
                                             </li>
                                         </ul>
                                     </div>
                                 </li>
-                                <li>
+                                <li className='menu-item relative py-3'>
                                     <Link href='/' className='flex items-center gap-1 group'>
                                         <span>Support</span>
                                         <ChevronDown color='white' size={20} className='chevron-rotate' />
                                     </Link>
                                     <div className='floating-menu bgc-o-90'>
-                                        <ul className='flex flex-col gap-4'>
+                                        <ul className='flex flex-col'>
                                             <li>
-                                                <Link href='/'>
-                                                    <span>FAQ</span>
+                                                <Link
+                                                    href='/'
+                                                    className='flex gap-2 px-5 items-center text-center leading-10 group'
+                                                >
+                                                    <MessageSquareText
+                                                        size={17}
+                                                        className='color-g50 group-hover:text-[#2aea65]'
+                                                    />
+                                                    <span className='color-g50 group-hover:text-[#2aea65]'>FAQ</span>
+                                                    <ArrowUpRight
+                                                        size={17}
+                                                        className='color-g50 group-hover:text-[#2aea65]'
+                                                    />
                                                 </Link>
                                             </li>
                                             <li>
-                                                <Link href='/'>
-                                                    <span>Contact Us</span>
+                                                <Link
+                                                    href='/'
+                                                    className='flex gap-2 px-5 items-center text-center whitespace-nowrap leading-10 group'
+                                                >
+                                                    <Send size={17} className='color-g50 group-hover:text-[#2aea65]' />
+                                                    <span className='color-g50 group-hover:text-[#2aea65]'>
+                                                        Contact Us
+                                                    </span>
                                                 </Link>
                                             </li>
                                         </ul>
                                     </div>
                                 </li>
-                                <li>
+                                <li className='menu-item relative py-3'>
                                     <Link href='/' className='flex items-center gap-1'>
                                         <span className='hover-line'>Collection</span>
                                         <ArrowUpRight color='white' size={20} />
